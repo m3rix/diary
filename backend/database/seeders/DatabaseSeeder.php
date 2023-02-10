6a5ebecaa712045b51cc88acbec93be82c007fd3
mikaelelg <mikael.elg.87@gmail.com>
2023-02-10 22:12:41 +0100

    Init commit

---
+++ backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use \App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+             'name' => 'Writer',
+             'email' => 'writer@example.com',
+            'password' => Hash::make('passcode')
+         ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('admin'),
+            'role' => 2
+        ]);
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@test.com',
+            'password' => Hash::make('secret'),
+            'role' => 1
+        ]);
+    }
+}
